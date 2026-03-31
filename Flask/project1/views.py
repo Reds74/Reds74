b44857bf1e51cb73e3fc8798175b59777786b9f3
Renê Soares <31143383+Reds74@users.noreply.github.com>
2026-03-31 20:11:40 -0300

    Add files via upload

--- Flask/project1/views.py
+++ Flask/project1/views.py
@@ -1,6 +1,10 @@
 from project1 import app
-from flask import render_template
+from flask import render_template, url_for
 
 @app.route('/')
 def homepage():
     return render_template('index.html')
+
+@app.route('/contato/')
+def newpage():
+    return 'Outra VIEW!!'
